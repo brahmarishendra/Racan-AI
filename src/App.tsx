295ed0703c7c0783922a6767277f1948a952f8be
Brahmarishendra.K <121884148+brahmarishendra@users.noreply.github.com>
2025-06-24 18:05:03 +0530

    Fix Authentication and Routing Issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Signup from './components/Login';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() { 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
